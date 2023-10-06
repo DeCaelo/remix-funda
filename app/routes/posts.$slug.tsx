ee0320a54f54ea5c06599348ac70412c8f63ae21
DeCaelo <ludovic.cleuet@gmail.com>
2023-10-06 11:31:17 +0200

    rendering html from markdown

--- app/routes/posts.$slug.tsx
+++ app/routes/posts.$slug.tsx
@@ -1,21 +1,27 @@
 import { LoaderFunctionArgs, json } from "@remix-run/node";
 import { useLoaderData } from "@remix-run/react";
+import { marked } from "marked";
 import { getPost } from "~/models/post.server";
 
 export async function loader({ request, params }: LoaderFunctionArgs) {
-  console.log(params);
+  if (!params.slug) {
+    throw new Error("missing slug");
+  }
   const post = await getPost(params.slug);
-  return json({ post });
+  if (!post) {
+    throw new Error("Post not found");
+  }
+  const html = marked(post.markdown);
+  return json({ title: post.title, html });
 }
 
 export default function PostRoute() {
-  const data = useLoaderData<typeof loader>();
+  const { title, html } = useLoaderData<typeof loader>();
 
   return (
     <main className="mx-auto max-w-4xl">
-      <h1 className="my-6 border-b-2 text-center text-3xl">
-        {data.post.title}
-      </h1>
+      <h1 className="my-6 border-b-2 text-center text-3xl">{title}</h1>
+      <div dangerouslySetInnerHTML={{ __html: html }} />
     </main>
   );
 }
