344c085668067f49eca00d7afc855ad789b3140f
DeCaelo <ludovic.cleuet@gmail.com>
2023-10-06 10:50:39 +0200

    prisma

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
+```bash
+npx prisma migrate dev
+npx prisma db seed
+```
+
 # Remix Indie Stack
 
 ![The Remix Indie Stack](https://repository-images.githubusercontent.com/465928257/a241fa49-bd4d-485a-a2a5-5cb8e4ee0abf)
--- app/routes/posts._index.tsx
+++ app/routes/posts._index.tsx
@@ -1,17 +1,9 @@
 import { json } from "@remix-run/node";
 import { Link, useLoaderData } from "@remix-run/react";
+import { prisma } from "~/db.server";
 
 async function getPosts() {
-  return [
-    {
-      slug: "my-first-post",
-      title: "My First Post",
-    },
-    {
-      slug: "90s-mixtape",
-      title: "A Mixtape I Made Just For You",
-    },
-  ];
+  return prisma.post.findMany({ select: { title: true, slug: true } });
 }
 
 export async function loader() {
