e0c5fd1729cf1379b8076abb1edf2c130a4aa6f7
DeCaelo <ludovic.cleuet@gmail.com>
2023-10-09 13:30:13 +0200

    Post mutation

--- app/models/post.server.ts
+++ app/models/post.server.ts
@@ -1,9 +1,19 @@
 import { prisma } from "~/db.server";
 
+import type { Post } from "@prisma/client";
+
 export async function getPosts() {
   return prisma.post.findMany({ select: { title: true, slug: true } });
 }
 
 export async function getPost(slug: string) {
   return prisma.post.findUnique({ where: { slug } });
+}
+
+export async function createPost({
+  title,
+  slug,
+  markdown,
+}: Pick<Post, "title" | "slug" | "markdown">) {
+  return prisma.post.create({ data: { title, slug, markdown } });
 }
--- app/routes/posts.admin.new.tsx
+++ app/routes/posts.admin.new.tsx
@@ -1,3 +1,78 @@
+import { ActionFunctionArgs, json, redirect } from "@remix-run/node";
+import { Form, useActionData } from "@remix-run/react";
+import invariant from "tiny-invariant";
+import { Button } from "~/components/ui/button";
+import { Input } from "~/components/ui/input";
+import { Label } from "~/components/ui/label";
+import { Textarea } from "~/components/ui/textarea";
+import { createPost } from "~/models/post.server";
+
+export async function action({ request }: ActionFunctionArgs) {
+  const formData = await request.formData();
+  const title = formData.get("title");
+  const slug = formData.get("slug");
+  const markdown = formData.get("markdown");
+
+  const errors = {
+    title: title ? null : "Title is required",
+    slug: slug ? null : "Slug is required",
+    markdown: markdown ? null : "Markdown is required",
+  };
+
+  const hasErrors = Object.values(errors).some(Boolean);
+
+  if (hasErrors) {
+    return json({ errors });
+  }
+
+  invariant(typeof title === "string", "title must be a string");
+  invariant(typeof slug === "string", "slug must be a string");
+  invariant(typeof markdown === "string", "markdown must be a string");
+
+  await createPost({ title, slug, markdown });
+
+  return redirect("/posts/admin");
+}
+
+const inputGrid = "grid w-full gap-1.5 mb-3";
+const textErrors = "text-red-600 font-bold";
+
 export default function NewPostRoute() {
-  return <h2>New Post</h2>;
+  const actionData = useActionData<typeof action>();
+  return (
+    <Form method="post">
+      <div className={inputGrid}>
+        <Label htmlFor="title">Post Title</Label>
+        <Input type="text" name="title" placeholder="Title" />
+        {actionData?.errors?.title ? (
+          <em className={textErrors}>{actionData.errors.title}</em>
+        ) : null}
+      </div>
+
+      <div className={inputGrid}>
+        <Label htmlFor="slug">Post Slug</Label>
+        <Input type="text" name="slug" placeholder="Slug" />
+        {actionData?.errors?.slug ? (
+          <em className={textErrors}>{actionData.errors.slug}</em>
+        ) : null}
+      </div>
+
+      <div className={inputGrid}>
+        <Label htmlFor="markdown">Markdown</Label>
+        <Textarea
+          id="markdown"
+          rows={8}
+          name="markdown"
+          className="font-mono"
+        />
+        {actionData?.errors?.markdown ? (
+          <em className={textErrors}>{actionData.errors.markdown}</em>
+        ) : null}
+      </div>
+
+      <div className="text-right">
+        <Button type="submit">Create Post</Button>
+      </div>
+    </Form>
+  );
 }
